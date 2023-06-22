999c86a4d0205162e1ec5a71c8282063cbdd748e
Clara <zl@dynaikon.com>
2023-06-22 14:54:53 +0100

    WIP model: finish model construction

--- model.py
+++ model.py
@@ -55,7 +55,7 @@ def get_src_trg(
     
     # decoder input. As per the paper, it must have the same dimension as the 
     # target sequence, and it must contain the last value of src, and all
-    # values of trg_y except the last (i.e. it must be shifted right by 1)
+    # values of trg_y except the last
     trg = sequence[enc_seq_len-1:seq_len-1, :, :]
     
     assert trg.shape[0] == target_seq_len, "Length of trg does not match target sequence length"
@@ -65,4 +65,154 @@ def get_src_trg(
 
     assert trg_y.shape[0] == target_seq_len, "Length of trg_y does not match target sequence length"
 
-    return src, trg, trg_y # change size from [batch_size, target_seq_len, num_features] to [batch_size, target_seq_len]
+    return src, trg, trg_y
+
+
+def generate_square_subsequent_mask(dim1: int, dim2: int) -> Tensor:
+    """copied from https://pytorch.org/tutorials/beginner/transformer_tutorial.html
+
+    :param dim1: for both src and tgt masking, this must be target sequence length
+    :type dim1: int
+    :param dim2: for src masking this must be encoder sequence length (i.e. 
+    the length of the input sequence to the model), and for tgt masking, this must be target sequence length
+    :type dim2: int
+    :return: mask tensor of shape [dim1, dim2]
+    :rtype: Tensor
+    """
+    return torch.triu(torch.ones(dim1, dim2) * float('-inf'), diagonal=1)
+
+
+class TimeSeriesTransformer(nn.Module):
+    """This time series transformer model is based on the paper by
+    Wu et al (2020) and the blog post by Kasper G. A. Ludvigsen
+    https://towardsdatascience.com/how-to-make-a-pytorch-transformer-for-time-series-forecasting-69e073d4061e
+
+    :param nn: _description_
+    :type nn: _type_
+    """
+    def __init__(
+            self,
+            model_dim: int = 512,
+            enc_len: int = 30,
+            n_heads: int = 4,
+            dropout_encoder: float=0.15,
+            dropout_decoder: float=0.15,
+            n_encoder_layers: int = 4,
+            n_decoder_layers: int = 4,
+            dim_feedforward_encoder: int=2048,
+            dim_feedforward_decoder: int=2048
+        ) -> None:
+        super().__init__()
+        # embed univariate input
+        self.encoder_input_layer = nn.Linear(in_features=1, out_features=model_dim)
+        # position encoding
+        self.position_encoder = PositionalEncoding(model_dim, max_len=enc_len)
+        # encoder layer
+        encoder_layer = nn.TransformerEncoderLayer(
+            d_model=model_dim,
+            nhead=n_heads,
+            batch_first=False,
+            dim_feedforward=dim_feedforward_encoder,
+            dropout=dropout_encoder
+        )
+        # encoder block
+        self.encoder = nn.TransformerEncoder(
+            encoder_layer=encoder_layer,
+            num_layers=n_encoder_layers,
+            norm=None
+        )
+        # decoder input layer from trg sequence
+        self.decoder_input_layer = nn.Linear(in_features=1, out_features=model_dim)
+        # decoder layer
+        decoder_layer = nn.TransformerDecoderLayer(
+            d_model=model_dim,
+            nhead=n_heads,
+            batch_first=False,
+            dropout=dropout_decoder,
+            dim_feedforward=dim_feedforward_decoder
+        )
+        self.decoder = nn.TransformerDecoder(
+            decoder_layer=decoder_layer,
+            num_layers=n_decoder_layers,
+            norm=None
+        )
+
+        # final univariate output
+        self.final_fc = nn.Linear(in_features=model_dim, out_features=1)
+
+    def forward(
+            self,
+            src: Tensor,
+            trg: Tensor,
+            src_mask: Tensor,
+            trg_mask: Tensor
+    ) -> Tensor:
+        # encode
+        src = self.encoder_input_layer(src)  # [seq_len, batch_size, input_feature_dim] -> [seq_len, batch_size, model_dim]
+        print(f'After encoder embedding: src shape {src.shape}')
+        src = self.position_encoder(src)  # [seq_len, batch_size, model_dim] -> [seq_len, batch_size, model_dim]
+        print(f'After position encoding: src shape {src.shape}')
+        src = self.encoder(src)  # no masking because no padding used for this data
+        print(f'After encoder: src shape {src.shape}')
+        # decode
+        decoder_output = self.decoder_input_layer(trg)  # [trg_seq_len, batch_size. input_feature_dim] -> [trg_seq_len, batch_size, model_dim]
+        print(f'After decoder embedding: decoder output shape {decoder_output.shape}')
+        decoder_output = self.decoder(
+            tgt=decoder_output,
+            memory=src,
+            tgt_mask=trg_mask,
+            memory_mask=src_mask
+        )
+        print(f'After decoder: decoder output shape {decoder_output.shape}')
+        decoder_output = self.final_fc(decoder_output)
+        print(f'After final linear layer: decoder output shape {decoder_output.shape}')
+
+        return decoder_output
+
+
+
+############# TRAIN TEST #############
+data = torch.rand(35, 32, 1)  # [total_seq_len, batch_size, input_feature_dim]
+
+model_dim = 512
+n_heads = 8
+enc_len = 30
+trg_len = 5
+
+model = TimeSeriesTransformer()
+
+src_mask = generate_square_subsequent_mask(
+    dim1=trg_len,
+    dim2=enc_len
+)
+trg_mask = generate_square_subsequent_mask(
+    dim1=trg_len,
+    dim2=trg_len
+)
+
+src, trg, trg_y = get_src_trg(data, enc_len, trg_len)
+
+output = model(src, trg, src_mask, trg_mask)
+
+print(output.shape)
+
+######## INFERENCE TEST ##########3
+
+src = torch.rand(30, 32, 1)
+trg = torch.rand(2, 32, 1)  # [seq_len, batch_size, feature_dim] note that the first input should be the last known value
+
+dim1 = trg.shape[0]
+dim2 = src.shape[0]
+
+trg_mask_inf = generate_square_subsequent_mask(
+    dim1=dim1,
+    dim2=dim1
+    )
+
+src_mask_inf = generate_square_subsequent_mask(
+    dim1=dim1,
+    dim2=dim2
+    )
+model.eval()
+pred = model(src, trg, src_mask_inf, trg_mask_inf)
+print(pred.shape)
